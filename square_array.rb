cc7f9c9c44e0ad4c9a010e1582400fa2d0aa7859
Richard Cho <richardcho100@gmail.com>
2018-04-11 02:24:27 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,19 +1,18 @@
 require "pry"
 
 #this is with the #each method
-#def square_array(array)
-  # your code here
-#  array_of_squares = []
-#  array.each do |number|
-#    array_of_squares << number**2
-    #binding.pry
-#    return array_of_squares if array_of_squares.size == array.size
-#  end
-#end
+def square_array(array)
+ array_of_squares = []
+ array.each do |number|
+   array_of_squares << number**2
+    binding.pry
+   return array_of_squares if array_of_squares.size == array.size
+ end
+end
 
 #this is with the #collect method
-def square_array(array)
-  array.collect do |number|
-    number**2
-  end
-end
+# def square_array(array)
+#   array.collect do |number|
+#     number**2
+#   end
+# end
