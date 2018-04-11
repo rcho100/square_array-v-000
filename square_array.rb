04608e1794ee6ab672b03fbcecbcf0ea3169b7e6
Richard Cho <richardcho100@gmail.com>
2018-04-11 02:25:57 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
+
+#this is with the #each method
 def square_array(array)
-  # your code here
+ array_of_squares = []
+ array.each do |number|
+   array_of_squares << number**2
+   return array_of_squares if array_of_squares.size == array.size
+ end
 end
+
+#this is with the #collect method
+# def square_array(array)
+#   array.collect do |number|
+#     number**2
+#   end
+# end
