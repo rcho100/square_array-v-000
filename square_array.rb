bef5ffa4f6b1a80185f51255a2926f2f15b10734
Richard Cho <richardcho100@gmail.com>
2018-04-11 01:58:00 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 require "pry"
 def square_array(array)
   # your code here
+  array_of_squares = []
   array.each do |number|
-    array_of_squares = []
+
     squared_number = number**2
     array_of_squares << squared_number
     binding.pry
