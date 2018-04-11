7f12d2598eb5012a6ddd6d8d3450e41a2929eaed
Richard Cho <richardcho100@gmail.com>
2018-04-11 02:04:35 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,8 +3,8 @@ def square_array(array)
   # your code here
   array_of_squares = []
   array.each do |number|
-    squared_number = number**2
-    array_of_squares << squared_number
+    #squared_number = number**2
+    array_of_squares << number**2
     #binding.pry
     return array_of_squares if array_of_squares.size == array.size
   end
