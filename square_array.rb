58b790177c07c72110ee98f2df866d5f81b61069
Richard Cho <richardcho100@gmail.com>
2018-04-11 02:01:23 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,11 +2,12 @@ require "pry"
 def square_array(array)
   # your code here
   array_of_squares = []
+  counter = 0
   array.each do |number|
-
+    counter += 1
     squared_number = number**2
     array_of_squares << squared_number
     #binding.pry
-    return array_of_squares if array_of_squares.size == 3
+    return array_of_squares if array_of_squares.size == counter
   end
 end
